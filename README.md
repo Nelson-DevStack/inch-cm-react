0376e4edf39f7b8e294409b7506b41e33b3f22e9
Nelson C <n.devpro@protonmail.com>
2022-05-23 21:04:35 -0400

    README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# Inch to Cm Converter (React Version)
+📋 This simple calculator converts inch values to centimeters and vice-versa
+
+## Techs
+- React.js & HTML
+- CSS
+- TypeScript
+- JavaScript
+
+## How to use
+⌨️ Just type the value you want to convert and the HTML Input will be filled with the result
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import { useReducer } from 'react';
-import './App.css'
+import './App.css';
 import Card from './components/Card';
 
-type ReducerState = {
-  count: number,
-  showText: boolean
-}
-
-enum Actions{
-  INCREMENT = "INCREMENT",
-  toggleShowText = "toggleShowText",
-}
-
-type ReducerAction = {
-  type: Actions
-}
-
-const reducer = (state: ReducerState, action: ReducerAction) => {
-  switch(action.type){
-    case Actions.INCREMENT:
-      return { count: state.count + 1, showText: state.showText };
-    case Actions.toggleShowText:
-      return { count: state.count, showText: !state.showText };
-    default:
-      return state;
-  }
-}
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
-
   return (
     <div className="App">
       <div className="Container">
